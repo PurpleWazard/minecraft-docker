20e1f3112c2b4c86bd8ec628e3a129d25348ccd5
purple wazard <purplewazard@gmail.com>
2024-08-23 11:23:38 -0500

    lol

--- wireguard/server.sh
+++ wireguard/server.sh
@@ -5,8 +5,13 @@ PRIVATE_KEY_FILE="$CONFIG_DIR/privatekey"
 PUBLIC_KEY_FILE="$CONFIG_DIR/publickey"
 WG_CONFIG="$CONFIG_DIR/wg0.conf"
 WG_LOG_FILE="/etc/wireguard/wireguard.log"
-CLIENT_KEY=$(cat "$CLIENT_KEY")
+CLIENT_KEY=$(cat /run/secrets/client_pub_key)
 
+# Check if CLIENT_KEY is set
+if [ -z "$CLIENT_KEY" ]; then
+    echo "CLIENT_KEY is not set. Exiting."
+    exit 1
+fi
 
 # Function to generate keys
 generate_keys() {
@@ -22,39 +27,36 @@ else
     echo "Using existing WireGuard keys..."
 fi
 
-if [ -n "$CLIENT_KEY" ]; then
-    exit 1
-fi
-
 # Create WireGuard configuration if it doesn't exist
 if [ ! -f "$WG_CONFIG" ]; then
     echo "Creating WireGuard configuration..."
-    echo "[Interface]" >> "$WG_CONFIG"
-    echo "PrivateKey = $(cat $PRIVATE_KEY_FILE)" >> "$WG_CONFIG"
-    echo "Address = 10.0.0.1/24" >> "$WG_CONFIG"
-    echo "ListenPort = 51820" >> "$WG_CONFIG"
-    echo "[Peer]" >> /etc/wireguard/wg0.conf
-    echo "AllowedIPs = 10.0.0.2/32" >> /etc/wireguard/wg0.conf
-    echo "PublicKey = $CLIENT_KEY" >> /etc/wireguard/wg0.conf
+    cat <<EOF > "$WG_CONFIG"
+[Interface]
+PrivateKey = $(cat $PRIVATE_KEY_FILE)
+Address = 10.0.0.1/24
+ListenPort = $SERVER_PORT
 
+[Peer]
+PublicKey = $CLIENT_KEY
+AllowedIPs = 10.0.0.2/32
+EOF
 else
     echo "Using existing WireGuard configuration..."
 fi
-
 
 # Enable IP forwarding
 sysctl -w net.ipv4.ip_forward=1
 sysctl -w net.ipv6.conf.all.forwarding=1
 
 # Start the WireGuard interface
-echo "this is the publickey: $(cat $PUBLIC_KEY_FILE )"
 wg-quick up wg0
 
+# Create log file if it doesn't exist
 if [ ! -f "$WG_LOG_FILE" ]; then
     touch "$WG_LOG_FILE"
     echo "this is the log file" >> "$WG_LOG_FILE"
 fi
 
-
-sleep forever
+# Keep the container running
+tail -f /dev/null
 
